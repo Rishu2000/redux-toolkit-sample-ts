5de99647f0356c36fa3045be2ab540e4ba8f7633
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-11-27 23:13:08 +0530

    Implemented redux toolkit for the books and used useSelector and useDispatch to do so.

--- src/components/Books.tsx
+++ src/components/Books.tsx
@@ -1,24 +1,58 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useAppDispatch, useAppSelector } from '../app/hooks'
+import { addBook, bookType, removeBook } from '../features/counter/bookSlice';
 
 const Books: React.FC = () => {
+
+const [titleState, setTitleState] = useState('');
+const [discState, setDiscState] = useState('');
+const [priceState, setPriceState] = useState('');
+
+const books = useAppSelector((state) => state.books.books);
+const dispatch = useAppDispatch();
+
+const book:bookType = {
+    title: titleState,
+    disc:discState,
+    price: +priceState
+}
+
     return (
         <div>
         <div className="m-5 d-flex justify-content-between flex-wrap">
-           <div className="alert alert-info mx-5" style={{width:"300px"}}>
-                <h2>Title</h2>
-                <h5>Description</h5>
-                <h3>Price</h3>
-           </div>
+            {books.map((book, key) => (
+                <div key={key} className="alert alert-info mx-5" style={{width:"300px"}}>
+                        <h2>{book.title}</h2>
+                        <p>{book.disc}</p>
+                        <h3>Price: {book.price}</h3>
+                </div>
+            ))}
         </div>
         <div>
             <form className="mx-5"> 
                 <div className="d-flex">
-                    <input className="form-control mx-3" type="text" placeholder="Title"/>
-                    <input className="form-control mx-3" type="text" placeholder="Description"/>
-                    <input className="form-control mx-3" type="text" placeholder="Price"/>
+                    <input className="form-control mx-3" type="text" 
+                        onChange={(e) => setTitleState(e.target.value)}
+                        placeholder="Title"/>
+                    <input className="form-control mx-3" type="text" 
+                        onChange={(e) => setDiscState(e.target.value)}
+                        placeholder="Description"/>
+                    <input className="form-control mx-3" type="text" 
+                        onChange={(e) => setPriceState(e.target.value)}
+                        placeholder="Price"/>
                 </div>
-                <button className="btn btn-primary m-3">Add</button>
-                <button className="btn btn-danger m-3">Remove</button>
+                <button className="btn btn-primary m-3"
+                    onClick={(e) => {
+                        e.preventDefault();
+                        dispatch(addBook(book))
+                    }}
+                >Add</button>
+                <button className="btn btn-danger m-3"
+                    onClick={(e) => {
+                        e.preventDefault();
+                        dispatch(removeBook(book))
+                    }}
+                >Remove</button>
             </form>
         </div>
         </div>
