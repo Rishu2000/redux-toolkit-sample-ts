b7f3ebb84601545840e254de8cb420a7ea1b0cca
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-11-27 18:37:41 +0530

    optimized the code and added a field to do increment as required.

--- src/features/counter/Counter.tsx
+++ src/features/counter/Counter.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useAppDispatch, useAppSelector } from '../../app/hooks'
 import { decrement, increment, incrementByAmount } from './counterSlice';
 
@@ -6,31 +6,23 @@ const Counter: React.FC = () => {
 
 const count = useAppSelector((state) => state.counter.value);
 const dispatch = useAppDispatch();
+const [val, setVal] = useState('');
 
-const handleIncrementClick:React.MouseEventHandler<HTMLButtonElement> = () => {
-    dispatch(increment());
-}
-
-const handleDecrementClick:React.MouseEventHandler<HTMLButtonElement> = () => {
-    dispatch(decrement());
-}
-
-const handleIncByNum:any = (num:number) => {
-    dispatch(incrementByAmount(num));
-}
+const newNum = +val;
 
     return (
         <div>
             <h2>Count is: {count}</h2>
             <button
-                onClick={handleIncrementClick}
+                onClick={() => dispatch(increment())}
             >Increment</button>
             <button
-                onClick={handleDecrementClick}
+                onClick={() => dispatch(decrement())}
             >Decerement</button>
+            <input type="number" placeholder="Increment by Number" onChange={(e) => setVal(e.target.value)}/>
             <button
-                onClick={() => handleIncByNum(100)}
-            >Incerement By 23</button>
+                onClick={() => dispatch(incrementByAmount(newNum))}
+            >Incerement By Number</button>
         </div>
     )
 }
