a8d7e87ef30a2692fbbb0e0b78627834253581a3
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-11-27 22:21:37 +0530

    Added some styling.

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -11,16 +11,21 @@ const [val, setVal] = useState('');
     return (
         <div>
             <h2>Count is: {count}</h2>
+            <div className="d-flex justify-content-center align-items-center">
             <button
+                className="btn btn-primary m-2"
                 onClick={() => dispatch(increment())}
             >Increment</button>
             <button
+                className="btn btn-primary m-2"
                 onClick={() => dispatch(decrement())}
             >Decerement</button>
-            <input type="number" placeholder="Increment by Number" onChange={(e) => setVal(e.target.value)}/>
+            <input className="form-control" style={{height:"100%", width:"200px"}} type="number" placeholder="Increment by Number" onChange={(e) => setVal(e.target.value)}/>
             <button
+                className="btn btn-primary m-2"
                 onClick={() => dispatch(incrementByAmount(+val))}
             >Incerement By Number</button>
+            </div>
         </div>
     )
 }
